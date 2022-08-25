d15ec33ab5d5075c708c8401ba6f1be192504918
aravindanand <aravind.anand@bourntec.com>
2022-08-25 10:41:12 +0530

    first commit

---
+++ src/main/java/com/bourntec/newz/NewzApplication.java
@@ -0,0 +1,13 @@
+package com.bourntec.newz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bourntec/newz/NewzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bourntec.newz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
